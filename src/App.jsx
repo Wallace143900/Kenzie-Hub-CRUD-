6a9bbda8d80700cb44b576c7114407e89c2d8f3d
Wallace143900 <hiperguardi@gmail.com>
2024-03-21 13:08:41 -0300

    Convetional commits

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { RouterMain } from "./pages/Routers/RouterMain/index.jsx";
 import "./styles/index.scss";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Dashboard } from "./pages/dashboard/index.jsx";
-import { Spinner } from "react-loading-io"
+import { Spinner } from "react-loading-io";
 import { DashboardContext } from "./providers/dashboardContext.jsx";
 
 
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss"
 export const Footer = () => {
     return (
         <footer>
-            <p className="headline">&copy; Todos os direitos reservados - Wallace Menezes</p>
+            <p className="headline" id={styles.p}>&copy; Todos os direitos reservados - Wallace Menezes</p>
         </footer>
     )
 }
