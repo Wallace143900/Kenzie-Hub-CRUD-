27922a04204dd82f53f68ca1f63a9941a5228b75
Wallace143900 <hiperguardi@gmail.com>
2024-03-28 13:03:51 -0300

    estava esquecendo de colocar ; nas importações

--- src/components/DefaultTemplate/index.jsx
+++ src/components/DefaultTemplate/index.jsx
@@ -1,5 +1,5 @@
-import { Footer } from "../Footer"
-import { Header } from "../Header"
+import { Footer } from "../Footer";
+import { Header } from "../Header";
 
 export const DefaultTemplate = ({ children }) => {
     return (
